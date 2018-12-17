da0184fee7dbdeb23b196353897a7e8112f0a34e
nrublevsky <nickrublevsky@gmail.com>
2018-12-17 18:02:36 +0200

    Shit working yo

--- app/src/main/java/Base.java
+++ app/src/main/java/Base.java
@@ -1,10 +1,14 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 
 public class Base {
@@ -20,17 +24,22 @@ public class Base {
         caps.setCapability("udid", "emulator-5554");
         caps.setCapability("platformName", "Android");
         caps.setCapability("platformVersion", "8.0");
-        caps.setCapability("appPackage", "com.rozdoum.eventor.couchbase");
-        caps.setCapability("appActivity", "com.rozdoum.eventor.activities.MainActivity");
+        caps.setCapability("appPackage", "com.rozdoum.eventor.lpmd2018");
+        caps.setCapability("appActivity", "com.rozdoum.eventor.activities.SchedulerActivity");
  //       caps.setCapability("noReset", "true");
 
 
         URL url = new URL("http://0.0.0.0:4723/wd/hub");
         AndroidDriver driver = new AndroidDriver(url, caps);
+        WebDriverWait wait = new WebDriverWait(driver, 30);
 
         driver.launchApp();
-        driver.findElement(By.id("com.rozdoum.eventor.couchbase:id/action_search")).click();
-        driver.findElement(By.className("android.support.v7.app.ActionBar$Tab")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//android.widget.ImageButton[@content-desc=\"Open navigation drawer\"]")));
+//        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"Open navigation drawer\"]")).isDisplayed();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"Open navigation drawer\"]")).click();
+
+
+//        driver.findElement(By.className("android.support.v7.app.ActionBar$Tab")).click();
     }
 
 
